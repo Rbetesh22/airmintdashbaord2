298511e441d6b33ed46c22b7febbab35c2f36be7
Ralph <ralphbetesh@Ralphs-MacBook-Pro.local>
2022-08-19 17:42:16 -0400

    first commit

---
+++ resources/views/themes/tallstack/home.blade.php
@@ -0,0 +1,205 @@
+@extends('theme::layouts.app')
+
+@section('content')
+
+
+<!-- Section 1 -->
+<section class="h-auto bg-white">
+    <div class="px-10 py-24 mx-auto max-w-7xl">
+        <div class="w-full mx-auto text-center md:text-center">
+            <h1 class="mb-6 text-5xl font-extrabold leading-none max-w-5xl mx-auto tracking-normal text-gray-900 sm:text-6xl md:text-6xl lg:text-8xl md:tracking-tight"> The American Dream, <br class="lg:block hidden"> <span class="w-full text-transparent bg-clip-text bg-gradient-to-r lg:inline from-blue-500 to-red-500">reimagined.</span> </h1>
+            <p class="px-0 mb-6 text-lg text-gray-600 md:text-xl lg:px-24"> AirMint realigns the incentives between brands and creators. </p>
+        </div>
+    </div>
+</section>
+
+<!-- Section 2 -->
+<section class="bg-white">
+    <div class="px-8 py-8 mx-auto sm:py-10 lg:py-20 max-w-7xl">
+        <div class="relative py-6 overflow-hidden rounded-lg bg-gradient-to-r lg:py-12 md:px-6 lg:p-16 lg:flex lg:items-center lg:justify-between md:shadow-xl to-red-500 from-blue-500">
+            <div class="absolute top-0 right-0 hidden w-full -mt-20 transform rotate-45 translate-x-1/2 bg-white sm:block h-96 opacity-5"></div>
+            <div class="absolute top-0 left-0 hidden w-full -mt-20 transform rotate-45 -translate-x-1/2 bg-pink-300 sm:block h-96 opacity-5"></div>
+            <div class="relative p-6 rounded-lg md:p-0 md:pb-4">
+                <h2 class="text-3xl font-extrabold leading-9 tracking-tight text-white sm:text-4xl sm:leading-10">Create your Brand Community.</h2>
+                <p class="w-full mt-5 text-base leading-6 text-pink-100 md:w-3/4">Buckle up and fly with Us!</p>
+            </div>
+            <div class="relative flex flex-col items-center w-full px-6 space-y-5 md:space-x-5 md:space-y-0 md:flex-row md:w-auto lg:flex-shrink-0 md:px-0">
+                <a href="#_" class="block w-full px-5 py-3 text-base font-medium leading-6 text-center transition duration-150 ease-in-out rounded-md md:inline-flex md:shadow md:w-auto hover:bg-blue-100 focus:outline-none focus:shadow-outline text-blue-600 bg-white">Sign Up</a>
+                <a href="#_" class="text-white">Log In</a>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+<!-- iPhone Test -->
+
+<section class="relative">
+                            <div class="max-w-6xl mx-auto px-4 sm:px-6 mt-12">
+                                <div class="py-12 md:py-20 border-t border-gray-200 dark:border-gray-800">
+            
+                                    <!-- Section header -->
+                                    <div class="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+                                        <h2 class="h2 font-red-hat-display mb-4 text-2xl font-bold leading-7"> We will help you build lasting communities.</h2>
+                                        <p class="text-xl text-gray-600 dark:text-gray-400">AirMint is a tool designed to power the next renaissance in branding. Our enterprise-grade mobile platform is the canvas to build, curate, and ship your successful marketing strategy using TikTok. The foremost priority of our team is to make your brands road to the future, simple, safe, and secure.</p>
+                                    </div>
+            
+                                    <!-- Glow illustration -->
+                                    <svg class="absolute left-1/2 transform -translate-x-1/2 mt-20 lg:mt-40 pointer-events-none -z-1 dark:opacity-20 hidden md:block" aria-hidden="true" width="854" height="509" viewBox="0 0 854 509" xmlns="http://www.w3.org/2000/svg">
+                                        <defs>
+                                            <radialGradient cx="50%" cy="50%" fx="50%" fy="50%" r="39.386%" id="iphonesill__a">
+                                                <stop stop-color="#667EEA" offset="0%" />
+                                                <stop stop-color="#667EEA" stop-opacity="0" offset="100%" />
+                                            </radialGradient>
+                                            <radialGradient cx="50%" cy="50%" fx="50%" fy="50%" r="39.386%" id="iphonesill__b">
+                                                <stop stop-color="#9F7AEA" offset="0%" />
+                                                <stop stop-color="#9F7AEA" stop-opacity="0" offset="100%" />
+                                            </radialGradient>
+                                        </defs>
+                                        <g transform="translate(-64 -64)" fill="none" fill-rule="evenodd">
+                                            <circle fill-opacity=".64" fill="url(#iphonesill__a)" cx="300" cy="300" r="300" />
+                                            <circle fill-opacity=".72" fill="url(#iphonesill__b)" cx="729" cy="384" r="240" />
+                                        </g>
+                                    </svg>
+            
+                                    <!-- Items -->
+                                    <div class="max-w-sm mx-auto grid gap-12 md:gap-x-6 lg:gap-x-12 md:grid-cols-3 items-start md:max-w-none">
+            
+                                        <!-- 1st item -->
+                                        <div class="relative flex flex-col items-center">
+                                            <!-- Line connecting (1) and (2) -->
+                                            <div aria-hidden="true" class="absolute h-px opacity-50 bg-gradient-to-r from-white via-teal-400 to-white dark:from-gray-900 dark:via-teal-400 dark:to-gray-900 hidden md:block" style="width:calc(100% - 48px);left:calc(50% + 48px);top:24px;">
+                                                <div class="absolute inset-0 border-t border-dashed border-white dark:border-gray-900"></div>
+                                            </div>
+                                            <!-- Number -->
+                                            <div class="w-12 h-12 rounded-full bg-teal-400 dark:bg-teal-600 dark:bg-opacity-25 flex justify-center items-center text-white dark:text-teal-400 font-medium mb-8 lg:mb-12">1</div>
+                                            <!-- Mobile mockup -->
+                                            <div class="inline-flex relative justify-center items-center">
+                                                <!-- Image inside mockup size: 290x624px (or 580x1248px for Retina devices) -->
+                                                <img class="absolute" src="storage/settings/May2021/images/mockup-image-02.jpg" width="290" height="624" style="max-width: 84.33%;" alt="App screen 02" />
+                                                <!-- iPhone mockup -->
+                                                <img class="relative max-w-full mx-auto h-auto pointer-events-none" src="storage/settings/May2021/images/iphone-mockup.png" width="344" height="674" alt="iPhone mockup" aria-hidden="true" />
+                                            </div>
+                                        </div>
+            
+                                        <!-- 2nd item -->
+                                        <div class="relative flex flex-col items-center">
+                                            <!-- Line connecting (2) and (3) -->
+                                            <div aria-hidden="true" class="absolute h-px opacity-50 bg-gradient-to-r from-white via-teal-400 to-white dark:from-gray-900 dark:via-teal-400 dark:to-gray-900 hidden md:block" style="width:calc(100% - 48px);left:calc(50% + 48px);top:24px;">
+                                                <div class="absolute inset-0 border-t border-dashed border-white dark:border-gray-900"></div>
+                                            </div>
+                                            <!-- Number -->
+                                            <div class="w-12 h-12 rounded-full bg-teal-400 dark:bg-teal-600 dark:bg-opacity-25 flex justify-center items-center text-white dark:text-teal-400 font-medium mb-8 lg:mb-12">2</div>
+                                            <!-- Mobile mockup -->
+                                            <div class="inline-flex relative justify-center items-center">
+                                                <!-- Image inside mockup size: 290x624px (or 580x1248px for Retina devices) -->
+                                                <img class="absolute" src="storage/settings/May2021/images/mockup-image-03.jpg" width="290" height="624" style="max-width: 84.33%;" alt="App screen 03" />
+                                                <!-- iPhone mockup -->
+                                                <img class="relative max-w-full mx-auto h-auto pointer-events-none" src="storage/settings/May2021/images/iphone-mockup.png" width="344" height="674" alt="iPhone mockup" aria-hidden="true" />
+                                            </div>
+                                        </div>
+            
+                                        <!-- 3rd item -->
+                                        <div class="relative flex flex-col items-center">
+                                            <div class="w-12 h-12 rounded-full bg-teal-400 dark:bg-teal-600 dark:bg-opacity-25 flex justify-center items-center text-white dark:text-teal-400 font-medium mb-8 lg:mb-12">3</div>
+                                            <!-- Mobile mockup -->
+                                            <div class="inline-flex relative justify-center items-center">
+                                                <!-- Image inside mockup size: 290x624px (or 580x1248px for Retina devices) -->
+                                                <img class="absolute" src="storage/settings/May2021/images/mockup-image-04.jpg" width="290" height="624" style="max-width: 84.33%;" alt="App screen 04" />
+                                                <!-- iPhone mockup -->
+                                                <img class="relative max-w-full mx-auto h-auto pointer-events-none" src="storage/settings/May2021/images/iphone-mockup.png" width="344" height="674" alt="iPhone mockup" aria-hidden="true" />
+                                            </div>
+                                        </div>
+            
+                                    </div>
+                                
+                                </div>
+                            </div>
+                        </section>
+
+
+<!-- Section 4 -->
+<section class="bg-white py-16">
+
+    <div class="container m-auto px-6 space-y-8 text-gray-500 md:px-12 lg:px-20">
+        <div class="flex items-center justify-center -space-x-3">
+            <img class="w-12 h-12 object-cover rounded-full border-4 border-white" src="https://cdn.devdojo.com/tails/avatars/094.jpg" alt="avatar">
+            <img class="w-16 h-16 object-cover rounded-full border-4 border-white" src="https://cdn.devdojo.com/tails/avatars/017.jpg" alt="avatar">
+            <img class="z-10 w-20 h-20 object-cover rounded-full border-4 border-white" src="https://cdn.devdojo.com/tails/avatars/080.jpg" alt="avatar">
+            <img class="relative w-16 h-16 object-cover rounded-full border-4 border-white" src="https://cdn.devdojo.com/tails/avatars/075.jpg" alt="avatar">
+            <img class="w-12 h-12 object-cover rounded-full border-4 border-white" src="https://cdn.devdojo.com/tails/avatars/082.jpg" alt="avatar">
+        </div>
+        <div class="m-auto space-y-7 w-full pb-5 max-w-2xl">
+            <h1 class="text-4xl text-gray-700 font-bold text-center md:text-5xl poppins">Start Building Today</h1>
+            <p class="text-xl text-center poppins">Join us and start building your brand communities!</p>
+            <div class="flex flex-wrap justify-center gap-4">
+                <a href="#_" title="Get started" class="block w-full py-4 px-8 text-center rounded-full bg-blue-600 text-lg transition hover:bg-blue-700 active:bg-blue-800 focus:bg-sky-600 sm:w-max">
+                    <span class="text-white font-semibold poppins">Enter the Future</span>
+                </a>
+                <a href="#_" title="More" class="block w-full py-4 px-8 text-center rounded-full text-lg bg-slate-100 transition hover:bg-slate-200 active:bg-slate-300 focus:bg-slate-200 sm:w-max">
+                    <span class="text-cyan-700 font-semibold poppins">Learn More</span>
+                </a>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+<!-- Section 5 -->
+<section class="box-border relative block py-16 overflow-hidden leading-6 text-left bg-no-repeat bg-cover md:py-20">
+    <div class="w-full px-4 px-8 mx-auto leading-6 text-left xl:px-0 max-w-7xl xl:px-24">
+        <div class="flex flex-wrap px-12 pt-8 pb-12 -mx-4 border-t border-b border-blue-200 opacity-100 xl:px-0">
+            <div class="relative w-full px-4 leading-6 text-left xl:flex-grow-0 xl:flex-shrink-0 lg:flex-grow-0 lg:flex-shrink-0"></div>
+            <div class="absolute w-full px-4 mt-2 -ml-10 leading-6 text-left md:flex-grow-0 md:flex-shrink-0">
+            <svg class="text-blue-200 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" height="24"><path d="M9.983 3v7.391C9.983 16.095 6.252 19.961 1 21l-.995-2.151C2.437 17.932 4 15.211 4 13H0V3h9.983zM24 3v7.391c0 5.704-3.748 9.571-9 10.609l-.996-2.151C16.437 17.932 18 15.211 18 13h-3.983V3H24z" class=""></path></svg>
+            </div>
+            <div class="relative w-full px-4 leading-6 text-left xl:flex-grow-0 xl:flex-shrink-0 lg:flex-grow-0 lg:flex-shrink-0">
+                <div class="box-border pt-2 text-lg md:text-xl">
+                   The best way to level-up your brand.
+                </div>
+                <div class="box-border flex items-center mt-5 text-indigo-700">
+                    <img src="https://cdn.devdojo.com/tails/avatars/051.jpg" class="w-16 h-16 leading-6 text-left align-middle border-none rounded-full">
+                    <div class="ml-4 text-sm font-semibold text-left text-gray-700 uppercase">
+                        Ralph Betesh, AirMint
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Section 6 -->
+<section class="relative py-16 bg-white min-w-screen animation-fade animation-delay">
+    <div class="container px-0 px-8 mx-auto sm:px-12 xl:px-5">
+        <p class="text-xs font-bold text-left uppercase sm:mx-6 sm:text-center sm:text-normal sm:font-bold text-red-500">
+            Got a Question? We’ve got answers.
+        </p>
+        <h3 class="mt-1 text-2xl font-bold text-left text-gray-800 sm:mx-6 sm:text-3xl md:text-4xl lg:text-5xl sm:text-center sm:mx-0">
+            Frequently Asked Questions
+        </h3>
+        <div class="w-full px-6 py-6 mx-auto mt-10 bg-white border border-gray-200 rounded-lg sm:px-8 md:px-12 sm:py-8 sm:shadow lg:w-5/6 xl:w-2/3">
+            <h3 class="text-lg font-bold sm:text-xl md:text-2xl text-blue-500">How does it work?</h3>
+            <p class="mt-2 text-base text-gray-600 sm:text-lg md:text-normal">
+            By promoting a brand, creators will directly amplify organic marketing efforts. They will create-to-earn, collecting varied amounts of rewards and benefits dependent on level of contribution and engagement rates.
+            </p>
+        </div>
+        <div class="w-full px-6 py-6 mx-auto mt-10 bg-white border border-gray-200 rounded-lg sm:px-8 md:px-12 sm:py-8 sm:shadow lg:w-5/6 xl:w-2/3">
+            <h3 class="text-lg font-bold sm:text-xl md:text-2xl text-blue-500">How much does it cost?</h3>
+            <p class="mt-2 text-base text-gray-600 sm:text-lg md:text-normal">
+                Nothing, AirMint is free to use! We charge 1% on every transaction, that's it.
+            </p>
+        </div>
+        <div class="w-full px-6 py-6 mx-auto mt-10 bg-white border border-gray-200 rounded-lg sm:px-8 md:px-12 sm:py-8 sm:shadow lg:w-5/6 xl:w-2/3">
+            <h3 class="text-lg font-bold sm:text-xl md:text-2xl text-blue-500">How do I start my campaign?</h3>
+            <p class="mt-2 text-base text-gray-600 sm:text-lg md:text-normal">
+                You can easily customize your campaign within our Brand Dashboard. Just create your free account to get started!
+            </p>
+        </div>
+     
+    </div>
+</section>
+
+
+
+@endsection
---
+++ storage/app/public/settings/May2021/js/main.js
@@ -0,0 +1,8 @@
+AOS.init({
+  once: true,
+  disable: 'phone',
+  duration: 700,
+  easing: 'ease-out-cubic',
+});
+
+const sticky = new Sticky('[data-sticky]');
